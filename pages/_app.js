811361fbefc9076c71e2032a8696c7867d58cbd9
Ali Ibrahim <alialchi07@gmail.com>
2023-07-11 14:07:52 -0500

    Set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Home from ".";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [render, setRender] = useState(false);
+  useEffect(() => setRender(true), []);
+  return render ? <Home /> : <div>Loading...</div>;
 }
